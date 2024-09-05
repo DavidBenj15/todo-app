65b84a6fb78bd73a362d147a3b76fabc04cd8869
David Benjamin <davidnbenjamin15@gmail.com>
2024-09-05 08:44:13 -0400

    Set up the layout and styling

--- src/main.js
+++ src/main.js
@@ -4,7 +4,4 @@ the content of the file into a new main.js file created using
 my GUI. */
 import "../style.css";
 
-const app = document.getElementById("app");
-app.innerHTML = `<div class="flex items-center justify-center h-screen">
-  <div>Hello World!</div>
-</div>`;
+
