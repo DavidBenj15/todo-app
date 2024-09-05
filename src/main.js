d3b162c5d7166f09ebe982b8a222c2bdf50c46ce
David Benjamin <davidnbenjamin15@gmail.com>
2024-09-04 21:34:13 -0400

    Initial commit: Set up project with Vite, Tailwind CSS, and Prettier

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
+/* note: I encountered errors when trying to serve main.js after
+it was created using echo "" > main.js. I fixed this by pasting
+the content of the file into a new main.js file created using
+my GUI. */
 import "../style.css";
+
+const app = document.getElementById("app");
+app.innerHTML = `<div class="flex items-center justify-center h-screen">
+  <div>Hello World!</div>
+</div>`;
--- vite.config.js
+++
@@ -1,5 +0,0 @@
-import { defineConfig } from "vite";
-
-export default defineConfig({
-  base: "/cw-04-todo-app-starter/", /* TODO: update to your repository name */
-});
